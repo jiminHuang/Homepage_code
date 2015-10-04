421ac31ad703e38ea8f903709ca7ea1f931f2f89
Jimin Huang <windworship2@163.com>
2015-10-04 15:45:20 +0800

    调试信息

--- handler.py
+++ handler.py
@@ -269,6 +269,7 @@ class ProjectsHandler(BaseHandler):
     '''
     def get(self):
         projects = database.Project.query()
+        print projects
         
         if projects:
             projects = [database.Project.chew(project) for project in projects]

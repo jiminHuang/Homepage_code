178cf85bc9acdcb511c7e83bfae32c3c609fdfa3
Jimin Huang <windworship2@163.com>
2015-10-11 16:37:04 +0800

    test测试目录 pep8格式检查

--- test/test_chewer.py
+++ test/test_chewer.py
@@ -5,60 +5,61 @@
 # Created Time: 2015年10月03日 星期六 15时43分22秒
 #
 import chewer
-from nose.tools import *
-import mock
-import logging
+from nose.tools import assert_raises, assert_equal
 from datetime import datetime
 
 '''
     后续处理方法测试文件
 '''
 
+
 def test_static_image():
-    #image未输入
+    # image未输入
     assert_raises(TypeError, chewer.static_image)
-    
-    #image输入为空
+
+    # image输入为空
     assert chewer.static_image(None) is None
-    
-    #默认输入image
+
+    # 默认输入image
     assert_equal(
         chewer.static_image('test'),
         'img/test.jpeg'
     )
-    
-    #测试输入image其他后缀
+
+    # 测试输入image其他后缀
     assert_equal(
         chewer.static_image('test', 'png'),
         'img/test.png'
     )
 
+
 def test_strftime_present():
-    #异常输入
+    # 异常输入
     assert_raises(TypeError, chewer.strftime_present)
     assert_raises(TypeError, chewer.strftime_present, 1)
     assert_equal(chewer.strftime_present(None, 1), 'Unknown')
-    
-    #正常输入
+
+    # 正常输入
     assert_equal(
         chewer.strftime_present("%m/%Y", datetime(2010, 04, 06)),
         '04/2010'
-    );
-    
-    #persent
+    )
+
+    # persent
     assert_equal(
         chewer.strftime_present("%m/%Y", None),
         'Present'
-    );
+    )
+
 
 def test_text_cutter():
-    #异常输入
+    # 异常输入
     assert_raises(TypeError, chewer.text_cutter)
     assert_raises(TypeError, chewer.text_cutter, 1)
     assert_equal(chewer.text_cutter(None, 1), '')
     assert_equal(chewer.text_cutter(3, 1), '')
-    
-    #正常输入
+
+    # 正常输入
     assert_equal(
         chewer.text_cutter('test', 1),
         't...'
--- test/test_config.py
+++ test/test_config.py
@@ -5,24 +5,24 @@
 # Created Time: 2015年10月03日 星期六 20时13分05秒
 #
 import config
-import os
-from nose.tools import *
+from nose.tools import assert_equal
 import mock
 import ConfigParser
 
+
 @mock.patch('os.environ.get')
 def test_getattr(mock_os):
-    #环境变量可获取
+    # 环境变量可获取
     mock_os.return_value = 'test'
     assert_equal(config.Config.TEST_CONFIG, 'test')
     mock_os.assert_called_with('TEST_CONFIG')
-    
-    #环境变量不可获取
+
+    # 环境变量不可获取
     mock_os.return_value = None
-    #config中不存在
+    # config中不存在
     assert_equal(config.Config.TEST_CONFIG, None)
     mock_os.assert_called_with('TEST_CONFIG', None)
-    #config中存在
+    # config中存在
     with mock.patch('ConfigParser.SafeConfigParser.get'):
         ConfigParser.SafeConfigParser.get.return_value = 'test'
         assert_equal(config.Config.TEST_CONFIG, 'test')
